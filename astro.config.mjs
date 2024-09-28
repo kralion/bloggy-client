62261b7cd701c5012420c52b7a4dca14bcb0538d
Brayan <joan300501@gmail.com>
2024-09-28 08:56:35 -0500

    Set up for the integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig, envField } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), tailwind()],
+  env: {
+    schema: {
+      STRAPI_URL: envField.string({
+        context: "server",
+        access: "public",
+      }),
+    },
+  },
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,13 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+const product = defineCollection({
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    name: z.string(),
+    stock: z.number(),
+    cover: z.string(),
+  }),
 });
 
-export const collections = { blog };
+export const collections = { product };
