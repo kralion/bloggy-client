974fed2060f224f9ce645ffbb806805653f7a542
Brayan <joan300501@gmail.com>
2024-10-23 12:52:44 -0500

    Update Astro and tailwind versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 // @ts-check
-import { defineConfig, envField } from "astro/config";
-import mdx from "@astrojs/mdx";
+import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 
-import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import mdx from "@astrojs/mdx";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [sitemap(), mdx(), tailwind()],
 
   output: "server",
   adapter: vercel(),
